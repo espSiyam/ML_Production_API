836416dbdecf55ac4569bd0a62740fa13ca6333f
Siyam <42336409+espSiyam@users.noreply.github.com>
2021-09-12 15:53:27 +0600

    Initial commit

---
+++ .ipynb_checkpoints/api-checkpoint.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/predict', methods=['POST'])
+def predict():
+     json_ = request.json
+     query_df = pd.DataFrame(json_)
+     query = pd.get_dummies(query_df)
+     prediction = lr.predict(query)
+     return jsonify({'prediction': list(prediction)})
